e4f307330e2c47b5f8936a753018c5adb9a6a496
Brooke Leinberger <brooke.j.leinberger@gmail.com>
2022-03-16 23:31:59 -0400

    IT FUCKING WORKS

--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -3,24 +3,47 @@
 using System.Net;
 using System.Net.Sockets;
 
-Socket Server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-Server.Connect(IPAddress.Parse("192.168.58.169"), 7777);
+
+class Program
+{
+    static int Clamp(int value, int upperBound, int lowerBound)
+    {
+        if (value < lowerBound)
+            return lowerBound;
+        if (value > upperBound)
+            return upperBound;
+
+        return value;
+    }
+    
+    static void Main()
+    {
+
+        Socket Server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        Server.Connect(IPAddress.Parse("192.168.58.169"), 7777);
 //Server.Connect(IPAddress.Parse("127.0.0.1"), 7777);
 
-string path = "/home/juno/transfer_test.jpg";
+        string path = "/home/juno/transfer_test.jpg";
 
-byte[] dataBuffer = new byte[1024];
+        byte[] dataBuffer = new byte[1024];
 
+        int fileSize = 1843200;
 
-for (int num = 0; num < 60; num++)
-{
-    using FileStream fs = new FileStream($"/home/juno/transfers/test{num}.jpg", FileMode.Create);
-    for (int count = 0; count < 1843200;)
-    {
-        int gros = Server.Receive(dataBuffer);
-        fs.Write(dataBuffer, 0, gros);
-        fs.Flush();
-        count += dataBuffer.Length;
+        for (int num = 0; num < 5; num++)
+        {
+            using FileStream fs = new FileStream($"/home/juno/transfers/test{num}.jpg", FileMode.Create);
+            Console.WriteLine("New File");
+            for (int count = 0; count < fileSize;)
+            {
+                int gros = Server.Receive(dataBuffer, 0, Clamp(fileSize - count, 1024, 0), SocketFlags.None);
+                //Console.WriteLine(gros);
+                fs.Write(dataBuffer, 0, gros);
+                fs.Flush();
+                count += gros;
+                Console.WriteLine(count);
+            }
+
+            fs.Dispose();
+        }
     }
-    fs.Dispose();
 }
